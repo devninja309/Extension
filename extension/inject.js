701a2edbdc9b9ddc35e085014f87941bbb0ca570
ryooyama <ryooyama.dev@gmail.com>
2022-06-25 09:15:31 -0700

    refresh inject function

--- extension/inject.js
+++ extension/inject.js
@@ -1,22 +1,28 @@
 Array.prototype.extend = function (other_array) {
   /* You should include a test to check whether other_array really is an array */
-  other_array.forEach(function(v) {this.push(v)}, this);
-}
+  other_array.forEach(function (v) {
+    this.push(v);
+  }, this);
+};
 
 const getCssSelectorShort = (el) => {
-  let path = [], parent;
-  while (parent = el.parentNode) {
-    let tag = el.tagName, siblings;
+  let path = [],
+    parent;
+  while ((parent = el.parentNode)) {
+    let tag = el.tagName,
+      siblings;
     path.unshift(
-      el.id ? `#${el.id}` : (
-        siblings = parent.children,
-        [].filter.call(siblings, sibling => sibling.tagName === tag).length === 1 ? tag :
-        `${tag}:nth-child(${1+[].indexOf.call(siblings, el)})`
-      )
+      el.id
+        ? `#${el.id}`
+        : ((siblings = parent.children),
+          [].filter.call(siblings, (sibling) => sibling.tagName === tag)
+            .length === 1
+            ? tag
+            : `${tag}:nth-child(${1 + [].indexOf.call(siblings, el)})`)
     );
     el = parent;
-  };
-  return `${path.join(' > ')}`.toLowerCase();
+  }
+  return `${path.join(" > ")}`.toLowerCase();
 };
 
 // const getCssSelector = (el) => {
@@ -31,13 +37,18 @@ const getCssSelectorShort = (el) => {
 const english = [];
 
 fetch("http://localhost:5000/selector")
-  .then(res => res.json())
+  .then((res) => res.json())
   .then((res) => {
     res.map((item) => {
-      english.extend([...document.querySelectorAll("[" + item.selector + "]")].map(el => 
-         ({[`${item.selector}="${el.getAttribute(item.selector)}"`] : [el.innerText, getCssSelectorShort(el)]})
-      ));
-    })
+      english.extend(
+        [...document.querySelectorAll("[" + item.selector + "]")].filter(e => e.childElementCount === 0 && e.innerHTML.length !== 0).map((el) => ({
+          [`${item.selector}="${el.getAttribute(item.selector)}"`]: [
+            el.innerText,
+            getCssSelectorShort(el)
+          ]
+        }))
+      );
+    });
     console.log(english);
     fetch("http://localhost:5000/extension", {
       method: "POST",
@@ -46,12 +57,10 @@ fetch("http://localhost:5000/selector")
       },
       body: JSON.stringify({ english })
     }).then();
-  })
 
-
+  });
 
 // var english = document.querySelector('[data-qa="title"]').textContent;
 
 // document.querySelector('[data-qa="title"]').textContent =
 //   "the way work should work";
-
